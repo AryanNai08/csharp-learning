21655fef0bd58e4e9a3aa9f16df243fc45b0c7c4
Nai Aryan <130205139+AryanNai08@users.noreply.github.com>
2026-02-03 12:37:18 +0530

    chore: initial C# learning structure

---
+++ Basics/CSharpBasics/01-HelloWorld/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasics._01_HelloWorld
+{
+    internal class HelloWorld
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Basics/CSharpBasics/Main.cs
@@ -0,0 +1,14 @@
+﻿using CSharpBasics._01_HelloWorld;
+using System;
+
+namespace CSharpBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld.Run();
+            Console.ReadLine();
+        }
+    }
+}
