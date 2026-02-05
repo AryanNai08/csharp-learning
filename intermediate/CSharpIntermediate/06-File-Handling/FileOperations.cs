d71b516e5db9ccf7cda68b5c1921e783945f2cdc
Nai Aryan <130205139+AryanNai08@users.noreply.github.com>
2026-02-05 21:33:30 +0530

    Completed file handling in C#

---
+++ intermediate/CSharpIntermediate/06-File-Handling/FileOperations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpIntermediate._06_File_Handling
+{
+    internal class FileOperations
+    {
+    }
+}
