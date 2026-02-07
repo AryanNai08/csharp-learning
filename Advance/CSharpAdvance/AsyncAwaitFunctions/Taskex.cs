d1ed6bb6f23c6c0ec39beb8fed0d90263f362d3d
Nai Aryan <130205139+AryanNai08@users.noreply.github.com>
2026-02-07 21:24:30 +0530

    Completed Task in async-await in C#

---
+++ Advance/CSharpAdvance/AsyncAwaitFunctions/Taskex.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAdvance.AsyncAwaitFunctions
+{
+    internal class Taskex
+    {
+        public static async Task Run()
+        {
+            int result = await GetNumber();
+            Console.WriteLine(result);
+        }
+
+        static async Task<int> GetNumber()
+        {
+            await Task.Delay(2000);
+            return 100;
+        }
+    }
+}
--- Advance/CSharpAdvance/Program.cs
+++ Advance/CSharpAdvance/Program.cs
@@ -12,9 +12,10 @@ namespace CSharpAdvance
     {
         static void Main(string[] args)
         {
-            AsyncAwait.Run().Wait(); //use wait because there is only single main method in whole project and dont make it async!!
-            CancellationTokenEx.Run().Wait();
-            MultipleAsyncTask.Run().Wait();
+            //AsyncAwait.Run().Wait(); //use wait because there is only single main method in whole project and dont make it async!!
+            //CancellationTokenEx.Run().Wait();
+            //MultipleAsyncTask.Run().Wait();
+            Taskex.Run().Wait();
         }
     }
 }
